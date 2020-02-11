7b900a729f3289c8969bd32baa5839f38b2b04c5
parkerhzb <1127289617@qq.com>
2020-02-11 20:10:27 +0800

    wx拍照登录；预约房间；访客记录添加

--- src/main/java/com/arcsoft/hotel/controller/checkinController.java
+++ src/main/java/com/arcsoft/hotel/controller/checkinController.java
@@ -7,14 +7,17 @@ import com.arcsoft.hotel.pojo.Room;
 import com.arcsoft.hotel.service.checkinService;
 import com.arcsoft.hotel.service.impl.checkinServiceImpl;
 import com.arcsoft.hotel.service.roomService;
+import com.arcsoft.hotel.util.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -26,7 +29,10 @@ public class checkinController {
     roomService roomService;
 
     @RequestMapping("/checkin")
-    public String checkinAll(Model model, @RequestParam("num") int num) {
+    public String checkinAll(Model model, @RequestParam("num") int num) throws IOException {
+        Resource resource = new ClassPathResource("");
+        FileUploadUtil fileUploadUtil = new FileUploadUtil();
+        String OriginPath = resource.getFile().getAbsolutePath();//D:\work\extra\十一届服务外包\AS_Hotel\target\classes
 //        checkInExample.createCriteria().andIsCheckOutIsNull();
 //        List<CheckIn> checkIns=checkInMapper.selectByExampleWithBLOBs(checkInExample);
         Room room = roomService.getRoombyId(1);
